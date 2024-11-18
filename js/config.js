16054c966b99214002e71eb6d161af07d761baae
Abhishek <abhishek1878sjs@gmail.com>
2024-11-18 21:38:42 +0530

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-north-1_X4Xc6NYmv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3sq07p13kkrejpfnq0sl6fm7q0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://hwxul1llsb.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
